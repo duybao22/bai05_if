bffd91c0da600d3a375c3ec8cc5799bc3323c94e
duybao22 <149235175+duybao22@users.noreply.github.com>
2024-01-19 15:46:15 +0700

    Add files via upload

---
+++ bai05_if/index.php
@@ -0,0 +1,26 @@
+<?php 
+ $t = date('H');
+ if($t < '10'){
+    echo 'have a good morning';
+ }elseif($t < '20'){
+    echo 'Have a good day';
+ }else{
+    echo 'Have a good night';
+ }
+ echo '<br>';
+ var_dump($t);
+ echo '<br>';
+ $a = 5;
+ $b = 6;
+ if($a == 0){
+    if($b == 0){
+        echo 'Phuong trinh co vo so nghiem';
+    }else{
+        echo 'Phuong trinh vo so nghiem';
+    }
+ }else{
+    $x = - $b / $a;
+    echo "Pt co nghiem: $x";
+ }
+
+?>
